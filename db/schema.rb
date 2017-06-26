0e837da7b6e593449cbd11c152069d0f6c0c32ce
Nandini Nayak <nandini@coderfactory.com>
2017-06-26 07:01:41 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322024116) do
+ActiveRecord::Schema.define(version: 20170525034255) do
 
   create_table "menus", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170322024116) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "delivery"
     t.index ["menu_id"], name: "index_orders_on_menu_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
